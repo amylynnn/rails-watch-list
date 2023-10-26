2da86a00182393a99c0738a997372a0a9d31b81e
amylynn sophie <amylynn7sophie@gmail.com>
2023-10-26 14:10:48 +0400

    ejj

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,13 @@
 class List < ApplicationRecord
-  belongs_to :movie
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  before_destroy :destroy_child_movies
+
+  private
+
+  def destroy_child_movies
+    movies.destroy_all
+  end
 end
