e12a371d8cc212c02c572906aa5a0aabadddb6e7
amylynn sophie <amylynn7sophie@gmail.com>
2023-10-26 11:10:22 +0400

    list

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_26_070535) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_070951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.string "string"
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_lists_on_movie_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_26_070535) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "lists", "movies"
 end
